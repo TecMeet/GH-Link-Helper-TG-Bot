408652e3aa98adc0e1e057b68742c94bc85243bf
timbze <tim.bze@gmail.com>
2022-06-23 15:57:08 -0600

    Parsing TG message text, checking GH for issue, and sending link back to TG works

--- GitHubLinkBot/src/GitHubLinkBot/EndPoints/PostTelegramUpdate.cs
+++ GitHubLinkBot/src/GitHubLinkBot/EndPoints/PostTelegramUpdate.cs
@@ -1,6 +1,8 @@
 using System.Text.RegularExpressions;
 using FastEndpoints;
 using GitHubLinkBot.Config;
+using Octokit;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
@@ -8,9 +10,22 @@ namespace GitHubLinkBot.EndPoints;
 
 public class PostTelegramUpdate : Endpoint<Update>
 {
+    private ITelegramBotClient TelegramClient { get; }
+    private IConfiguration Configuration { get; }
+    private IGitHubClient GitHubClient { get; }
+
+    public PostTelegramUpdate(ITelegramBotClient telegramClient, IConfiguration configuration, IGitHubClient gitHubClient)
+    {
+        TelegramClient = telegramClient;
+        Configuration = configuration;
+        GitHubClient = gitHubClient;
+    }
+    
     public override void Configure()
     {
-        var botToken = Config?.GetSection("BotConfiguration").Get<BotConfig>().BotToken;
+        // use FastEndpoints config because it has not been injected yet at this point
+        var config = Config!.GetSection("BotConfiguration").Get<BotConfig>();
+        var botToken = config.BotToken;
         Verbs(Http.POST);
         Routes($"bot/{botToken}");
         AllowAnonymous();
@@ -18,18 +33,40 @@ public class PostTelegramUpdate : Endpoint<Update>
 
     public override async Task HandleAsync(Update req, CancellationToken ct)
     {
-        Console.WriteLine("HandleAsync");
+        // if message is empty or not "Message" type then don't do anything
         var msg = req.Message?.Text;
         if (req.Type != UpdateType.Message ||
             string.IsNullOrWhiteSpace(msg)) 
             return;
         
-        Console.WriteLine($"Telegram message: {msg}");
-
         var matches = Regex.Matches(msg, "#[0-9]+");
+        if (!matches.Any()) return; // no GH issues are mentioned in the Telegram message (i.e. format #123)
+        
+        Console.WriteLine($"Telegram message: {msg}");
+        
+        var config = Configuration.GetSection("BotConfiguration").Get<BotConfig>();
+        var repoOwner = config.GitHubRepoOwner;
+        var repoName = config.GitHubRepoName;
+        
         foreach (var m in matches)
         {
+            if (string.IsNullOrWhiteSpace(m.ToString())) continue;
             Console.WriteLine($"Match found: {m}");
+            var issue = Convert.ToInt32(m.ToString()!.TrimStart('#'));
+            try
+            {
+                // check if GH issue exists, if not it will throw "NotFoundException"
+                var gIssue = await GitHubClient.Issue.Get(repoOwner, repoName, issue);
+                
+                // not awaiting because it's unnecessary and if there's more than 1 issue number it will get to adding
+                //  the next link faster
+                _ = TelegramClient.SendTextMessageAsync(req.Message!.Chat.Id, gIssue.HtmlUrl, cancellationToken:ct);
+            }
+            catch (NotFoundException) {} // issue not found on GH
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
--- GitHubLinkBot/src/GitHubLinkBot/Program.cs
+++ GitHubLinkBot/src/GitHubLinkBot/Program.cs
@@ -1,15 +1,22 @@
 using FastEndpoints;
 using GitHubLinkBot.Config;
 using GitHubLinkBot.Services;
+using Octokit;
 using Telegram.Bot;
 
 var builder = WebApplication.CreateBuilder(args);
 var botConfig = builder.Configuration.GetSection("BotConfiguration").Get<BotConfig>();
 
+// need to set configuration here because it can't be injected yet
 builder.Services.AddFastEndpoints(config: builder.Configuration);
 
-builder.Services.AddHostedService<ConfigureWebhook>();
+// configure webhook for Telegram bot on app Startup, and unregister hook on app shutdown
+builder.Services.AddHostedService<ConfigureTelegramWebhook>();
 
+// add GitHub client
+builder.Services.AddScoped<IGitHubClient>(_ => new GitHubClient(new ProductHeaderValue("GH-Link-Helper-Telegram-Bot")));
+
+// add Telegram client
 builder.Services.AddHttpClient("tg").AddTypedClient<ITelegramBotClient>(client =>
     new TelegramBotClient(botConfig.BotToken, client));
 
@@ -17,6 +24,7 @@ var app = builder.Build();
 
 app.UseFastEndpoints(config =>
 {
+    // set up for Newtonsoft serializer because Telegram does not yet support the current default System.Text.Json
     config.RequestDeserializer = async (req, tDto, jCtx, ct) =>
     {
         using var reader = new StreamReader(req.Body);
